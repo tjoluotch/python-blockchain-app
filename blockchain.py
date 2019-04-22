61de8d02665cb837c0c556a8f10a499d086b5820
TJ Oluotch <trevorjoluotch@gmail.com>
2019-04-22 01:19:20 +0100

    Added transaction amount as user input

--- blockchain.py
+++ blockchain.py
@@ -12,14 +12,18 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-tx_amount = float(input("Your transaction amount please: "))
+def get_user_input():
+    return float(input("Your transaction amount please: "))
+
+
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input("Your transaction amount please: "))
+tx_amount = get_user_input()
 # Keyword arguments
 add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
 
-tx_amount = float(input("Your transaction amount please: "))
+tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain)
